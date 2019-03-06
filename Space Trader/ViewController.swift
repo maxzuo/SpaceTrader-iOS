8ebe8f02c8740da9887ec819ae9bdf99b6bcafa8
Max Zuo <max.zuo@gmail.com>
2019-03-06 07:54:42 -0500

    Initial commit. Initialized Xcode projectwq

---
+++ Space Trader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Space Trader
+//
+//  Created by Zhaohui Li on 3/6/19.
+//  Copyright © 2019 Max Zuo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
